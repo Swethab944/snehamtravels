c292713264b31dc9ddb82c43bc719abf6f716c32
Swetha Balasubramaninan <143270097+Swethab944@users.noreply.github.com>
2024-05-15 18:29:17 +0530

    New traiff

--- index.js
+++ index.js
@@ -4,12 +4,11 @@ function showDetails(vehicle) {
                                 <h2>SEDAN (ZEST, DZIRE, ETIOS)</h2>
                                 <h3>Package (Day Rent Basis / Kilometer Basis)</h3>
                                 <h4>Day Rent Basis</h4>
-                                <p>Day Rent: Rs. 1800</p>
-                                <p>Fuel: Rs. 9</p>
-                                <p>Driver Batta: Rs. 300</p>
+                                <p>Day Rent: Rs. 2000</p>
+                                <p>Fuel: Rs. 10</p>
                                 <h4>Kilometer Basis</h4>
-                                <p>Fuel: Rs. 13</p>
-                                <p>Driver Batta: Rs. 300</p>
+                                <p>Fuel: Rs. 14</p>
+                                <p>Driver Batta: Rs. 400</p>
                             </div>`;
         document.getElementById('sedanDetails').innerHTML = sedanDetails;
         document.getElementById('sedanDetails').style.display = 'block';
@@ -20,11 +19,11 @@ function showDetails(vehicle) {
                                 <h3>Package (Day Rent Basis / Kilometer Basis)</h3>
                                 <h4>Day Rent Basis</h4>
                                 <p>Day Rent: Rs. 2500</p>
-                                <p>Fuel: Rs. 14</p>
-                                <p>Driver Batta: Rs. 400</p>
+                                <p>Fuel: Rs. 15</p>
+                                <p>Driver Batta: Rs. 500</p>
                                 <h4>Kilometer Basis</h4>
                                 <p>Fuel: Rs. 18</p>
-                                <p>Driver Batta: Rs. 400</p>
+                                <p>Driver Batta: Rs. 500</p>
                             </div>`;
         document.getElementById('innovaDetails').innerHTML = innovaDetails;
         document.getElementById('innovaDetails').style.display = 'block';
@@ -34,11 +33,11 @@ function showDetails(vehicle) {
                                 <h2>ERTIGA</h2>
                                 <h3>Package (Day Rent Basis / Kilometer Basis)</h3>
                                 <h4>Day Rent Basis</h4>
-                                <p>Day Rent: Rs. 2700</p>
-                                <p>Fuel: Rs. 16</p>
+                                <p>Day Rent: Rs. 2300</p>
+                                <p>Fuel: Rs. 14</p>
                                 <p>Driver Batta: Rs. 500</p>
                                 <h4>Kilometer Basis</h4>
-                                <p>Fuel: Rs. 20</p>
+                                <p>Fuel: Rs. 14</p>
                                 <p>Driver Batta: Rs. 500</p>
                             </div>`;
         document.getElementById('ertigaDetails').innerHTML = ertigaDetails;
@@ -49,7 +48,7 @@ function showDetails(vehicle) {
                                 <h2>CRYSTA</h2>
                                 <h3>Package (Day Rent Basis / Kilometer Basis)</h3>
                                 <h4>Day Rent Basis</h4>
-                                <p>Day Rent: Rs. 2700</p>
+                                <p>Day Rent: Rs. 3200</p>
                                 <p>Fuel: Rs. 16</p>
                                 <p>Driver Batta: Rs. 500</p>
                                 <h4>Kilometer Basis</h4>
